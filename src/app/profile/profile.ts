d31115c86a0d1ba4a64b3ab56b5f5daff801c739
BigOmi <saido8326@gmail.com>
2025-05-28 09:30:36 -0600

    tercer commit modificacion del ts

---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfiles',
+  templateUrl: './profile.html',
+  styleUrls: ['./profile.css']
+})
+export class PerfilesComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
