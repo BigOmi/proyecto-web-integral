f8d3c2465bc633a75cdb73c980f41654c128a4f6
Juan Antonio <11.juanantonio.102@gmail.com>
2025-05-28 09:35:26 -0600

    Creacion Modulo Usuarios

---
+++ src/app/users/perfiles/perfiles.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfiles',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfiles.component.html',
+  styleUrl: './perfiles.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PerfilesComponent { }
